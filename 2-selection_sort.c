2097561ee623d13db8b4087db4c29b32a516a49f
peluza <edisonisaza@gmail.com>
2020-06-12 15:58:19 -0500

    2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+void selection_sort(int *array, size_t size)
+{
+        size_t i, j;
+        int temp;
+
+        for (i = 0; i < size - 1; i++)
+        {
+                for (j = i + 1; j < size; j++)
+                {
+                        if (array[i] > array[j])
+                        {
+                                if (array[i] != array[j])
+                                {
+                                        temp = array[i];
+                                        array[i] = array[j];
+                                        array[j] = temp;
+                                }
+                        }
+                }
+                print_array(array, size);
+        }
+}
