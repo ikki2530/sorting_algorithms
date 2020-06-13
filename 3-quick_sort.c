124343d75ee7376e3e2de0f05e6a66a103d4d04f
Diego Gómez <dagomez2530@gmail.com>
2020-06-13 17:01:40 +0000

    Adding comments to functions

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,52 +1,68 @@
 #include "sort.h"
+/**
+ * swap_arr - swap two numbers in the array
+ * @a: number 1
+ * @b: number 2
+ * @array: array to print in case of swap
+ * @size: array size
+ * Return: Nothing
+ */
 void swap_arr(int *a, int *b, int *array, size_t size)
 {
-        int temp;
+	int temp;
 
-        temp = *a;
-        *a = *b;
-        *b = temp;
-        if (temp != *a)
-                print_array(array, size);
+	temp = *a;
+	*a = *b;
+	*b = temp;
+	if (temp != *a)
+		print_array(array, size);
 }
+/**
+ * partition - function to parse the array in 2 arrays
+ * @array: array
+ * @low: position of first element of the array
+ * @last: position of the last element
+ * @size: array size
+ * Return: final position of pivot
+ */
 int partition(int *array, int low, int last, size_t size)
 {
-        int pivot = array[last], j;
-        int i = low - 1;
+	int pivot = array[last], j;
+	int i = low - 1;
 
-        /*printf("last %d\n", array[last]);*/
-        for (j = low; j < last; j++)
-        {
-                /*printf("array[%d] = %d, pivot = %d\n", j, array[j], pivot);*/
-                if (array[j] <= pivot)
-                {
-                        /*printf("inside if\n");*/
-                        i++;
-                        swap_arr(&array[j], &array[i], array, size);
-                        /*print_array(array, size);*/
-                }
-        }
-        i = i + 1;
-        swap_arr(&array[i], &array[last], array, size);
-        /*printf("Last print of partition\n");*/
-        /*print_array(array,size);*/
-        return (i);
+	for (j = low; j < last; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			swap_arr(&array[j], &array[i], array, size);
+		}
+	}
+	i = i + 1;
+	swap_arr(&array[i], &array[last], array, size);
+	return (i);
 
 }
-
+/**
+ * new_quick - Recursive function to sort the array
+ * @array: array
+ * @low: position of first element of the array
+ * @last: position of the last element
+ * @size: array size
+ * Return: Nothing
+ */
 void new_quick(int *array, int low, int last, size_t size)
 {
-        int div;
+	int div;
 
-        if (low < last)
-        {
-                div = partition(array, low, last, size);
-                /*printf("low: %d, last= %d, div, %u, size: %lu\n\n", low, last, div, size);*/
-
-                new_quick(array, 0, div-1, size); /*left array*/
-                new_quick(array, div+1, last, size); /*right*/
-        }
+	if (low < last)
+	{
+		div = partition(array, low, last, size);
+		new_quick(array, 0, div - 1, size);
+		new_quick(array, div + 1, last, size);
+	}
 }
+
 /**
  * quick_sort - sorts an array in ascending order
  * @array: array to be sroted
@@ -55,9 +71,10 @@ void new_quick(int *array, int low, int last, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-        int last, low;
-        last = size - 1;
-        low = 0;
+	int last, low;
 
-        new_quick(array, low, last, size);
+	last = size - 1;
+	low = 0;
+
+	new_quick(array, low, last, size);
 }
