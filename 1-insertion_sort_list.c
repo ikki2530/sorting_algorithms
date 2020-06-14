5a3d836e2f0c5b8e77352bb876d626adcfbb1974
peluza <edisonisaza@gmail.com>
2020-06-14 15:14:06 -0500

    1-is

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp, *current;
 
-	if (*list == NULL || list == NULL)
+	if (list == NULL || *list == NULL)
 		return;
 	current = (*list)->next;
 	while (current)
